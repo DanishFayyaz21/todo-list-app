47ae8154128e9ea8f3ac9f261a070a0e6d90b7cb
DanishFayyaz21 <danishfayyaz91@gmail.com>
2023-08-14 23:32:45 +0500

    first commit

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-var mongoURL = process.env.DB_URL || "mongodb+srv://danishfayyaz:hwllo123@cluster0.q2jez5z.mongodb.net/?retryWrites=true&w=majority";
+var mongoURL = process.env.DB_URL;
 
 mongoose.connect(mongoURL, { useUnifiedTopology: true, useNewUrlParser: true });
 
